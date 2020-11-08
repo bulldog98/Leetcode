293583a80a85111711b466fdd10f5513ceb8afd1
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2020-11-08 14:06:20 +0100

    Improved runtime and memory consumption

--- src/main/kotlin/twelf_integer_to_roman/Solution.kt
+++ src/main/kotlin/twelf_integer_to_roman/Solution.kt
@@ -9,27 +9,32 @@ class Solution {
         val hundredDigit = (this % 1000) / 100
         val tenDigit = (this % 100) / 10
         val singleDigit = (this % 10)
-        return "${handleThousandsDigit(thousandDigit)}${handleHundreds(hundredDigit)}${handleTens(tenDigit)}${handleSingle(singleDigit)}"
+        val result = handleThousandsDigit(thousandDigit) +
+                handleHundreds(hundredDigit) +
+                handleTens(tenDigit) +
+                handleSingle(singleDigit)
+        return result.joinToString("")
     }
 
-    private fun handleThousandsDigit(num: Int): String {
-        if (num == 0) return ""
-        if (num <= 3) return (1..num).map { 'M' }.toCharArray().joinToString("")
+    private fun handleThousandsDigit(num: Int): CharArray {
+        if (num == 0) return CharArray(0)
+        if (num <= 3) return CharArray(num) { 'M' }
         throw IllegalArgumentException("Number to big")
     }
 
-    private fun handleHundreds(num: Int): String = handleFullDigit(num, 'C', "D", "M")
-    private fun handleTens(num: Int) = handleFullDigit(num, 'X', "L", "C")
-    private fun handleSingle(num: Int) = handleFullDigit(num, 'I', "V", "X")
+    private fun handleHundreds(num: Int) = handleFullDigit(num, 'C', 'D', 'M')
+    private fun handleTens(num: Int) = handleFullDigit(num, 'X', 'L', 'C')
+    private fun handleSingle(num: Int) = handleFullDigit(num, 'I', 'V', 'X')
 
-    private fun handleFullDigit(num: Int, singleSymbol: Char, fiveSymbol: String, tenSymbol: String): String {
+    private fun handleFullDigit(num: Int, singleSymbol: Char, fiveSymbol: Char, tenSymbol: Char): CharArray {
         require(num in 0..9) { "digit has to be in 0..9" }
-        if (num == 0) return ""
-        if (num < 4) return (1..num).map { singleSymbol }.toCharArray().joinToString("")
-        if (num == 4) return "$singleSymbol$fiveSymbol"
-        if (num < 9) return "$fiveSymbol${handleFullDigit(num - 5, singleSymbol, fiveSymbol, tenSymbol)}"
-        return "$singleSymbol$tenSymbol"
+        return when (num) {
+            0 -> CharArray(0)
+            in 1..3 -> CharArray(num) { singleSymbol }
+            4 -> CharArray(2) { if (it == 0) singleSymbol else fiveSymbol }
+            5 -> CharArray(1) { fiveSymbol }
+            in 6..8 -> CharArray(num - 4) { if (it == 0) fiveSymbol else singleSymbol }
+            else -> CharArray(2) { if (it == 0) singleSymbol else tenSymbol }
+        }
     }
 }
-
-
