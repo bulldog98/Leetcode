73b1766d49c7795adba803dfa9f758df07ee008a
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2020-11-08 16:04:19 +0100

    Used hint to drastically improve the runtime

--- src/main/kotlin/one_two_sum/Solution.kt
+++ src/main/kotlin/one_two_sum/Solution.kt
@@ -2,12 +2,13 @@ package de.bulldog98.leetcode.one_two_sum
 
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (i in 0..(nums.size - 2)) {
-            for (j in i + 1 until nums.size) {
-                if (nums[i] + nums[j] == target) {
-                    return intArrayOf(i, j)
-                }
+        val map = mutableMapOf<Int, Int>()
+        for (i in nums.indices) {
+            val complement = target - nums[i]
+            if (map.containsKey(complement)) {
+                return intArrayOf(map.getOrElse(complement) { throw IllegalArgumentException() }, i)
             }
+            map[nums[i]] = i
         }
         throw IllegalArgumentException("No match found")
     }
