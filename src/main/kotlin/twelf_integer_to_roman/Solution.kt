03b69feea3ca5f8bef3fa3e5e5c16c4b3d8a63df
Jonathan Kolberg <j_kolberg11@cs.uni-kl.de>
2020-11-08 14:44:44 +0100

    Improved speed to 188ms

--- src/main/kotlin/twelf_integer_to_roman/Solution.kt
+++ src/main/kotlin/twelf_integer_to_roman/Solution.kt
@@ -1,40 +1,22 @@
 package de.bulldog98.leetcode.twelf_integer_to_roman
 
 class Solution {
-    fun intToRoman(num: Int) = num.toRoman()
-
-    private fun Int.toRoman(): String {
-        require(this in 1..3999) { "number has to be between 1 and 3999 including these" }
-        val thousandDigit = this / 1000
-        val hundredDigit = (this % 1000) / 100
-        val tenDigit = (this % 100) / 10
-        val singleDigit = (this % 10)
-        val result = handleThousandsDigit(thousandDigit) +
-                handleHundreds(hundredDigit) +
-                handleTens(tenDigit) +
-                handleSingle(singleDigit)
-        return result.joinToString("")
-    }
-
-    private fun handleThousandsDigit(num: Int): CharArray {
-        if (num == 0) return CharArray(0)
-        if (num <= 3) return CharArray(num) { 'M' }
-        throw IllegalArgumentException("Number to big")
-    }
-
-    private fun handleHundreds(num: Int) = handleFullDigit(num, 'C', 'D', 'M')
-    private fun handleTens(num: Int) = handleFullDigit(num, 'X', 'L', 'C')
-    private fun handleSingle(num: Int) = handleFullDigit(num, 'I', 'V', 'X')
-
-    private fun handleFullDigit(num: Int, singleSymbol: Char, fiveSymbol: Char, tenSymbol: Char): CharArray {
-        require(num in 0..9) { "digit has to be in 0..9" }
-        return when (num) {
-            0 -> CharArray(0)
-            in 1..3 -> CharArray(num) { singleSymbol }
-            4 -> CharArray(2) { if (it == 0) singleSymbol else fiveSymbol }
-            5 -> CharArray(1) { fiveSymbol }
-            in 6..8 -> CharArray(num - 4) { if (it == 0) fiveSymbol else singleSymbol }
-            else -> CharArray(2) { if (it == 0) singleSymbol else tenSymbol }
+    fun intToRoman(num: Int): String {
+        require(num in 1..3999) { "number has to be between 1 and 3999 including these" }
+        var tmp = num
+        val sb = StringBuilder()
+        values.forEachIndexed { index, key ->
+            while (tmp >= key) {
+                sb.append(romanNumber[index])
+                tmp -= key
+            }
+            if (tmp == 0) return sb.toString()
         }
+        throw IllegalArgumentException("Something went wrong")
+    }
+
+    companion object {
+        private val values = arrayOf(1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+        private val romanNumber = arrayOf("M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I")
     }
 }
